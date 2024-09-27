26a2567336342c7aa291b1ba389f934c99f78856
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-09-27 13:50:33 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Niknock HD'
+description 'NKHD LAX (Replaces LSIA to LAX and Los Santos International Airport to Los Angeles International Airport)'
+version '1.0.0'
